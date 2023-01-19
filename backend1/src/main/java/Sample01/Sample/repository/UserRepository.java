7f4f5cbed1cc93924145170c11683e8949a98f77
skan <skan@pmirnc.com>
2023-01-19 18:51:29 +0900

    fix - backend1 delete

--- backend1/src/main/java/Sample01/Sample/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package Sample01.Sample.repository;
-
-import Sample01.Sample.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-    public User findUsersByUserId (String userId);
-}
